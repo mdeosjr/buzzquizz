aa1a315a2931321fffe642b4ac39cf18dd913d5c
Moises Junior <m.junior@eq.ufrj.br>
2021-11-18 17:29:27 -0300

    Tela de quizz escolhido em construção

--- buzzquizz.js
+++ buzzquizz.js
@@ -7,7 +7,6 @@ function receberQuizzes() {
 
 function listarQuizzes(info) {
     const quizzes = info.data
-    console.log(quizzes)
 
     const todosOsQuizzes = document.querySelector(".todosOsQuizzes")
 
@@ -15,7 +14,7 @@ function listarQuizzes(info) {
         let quizzUnico = quizzes[i]
         todosOsQuizzes.innerHTML += 
             `
-            <div class="quizz">
+            <div class="quizz" onclick="quizzIndividual()">
                 <img src="${quizzUnico.image}"/>
                 <span class="tituloQuizz">
                     ${quizzUnico.title}
@@ -26,3 +25,43 @@ function listarQuizzes(info) {
 }
 
 receberQuizzes();
+
+function quizzIndividual () {
+    const quizz = document.querySelector(".quizzIndividual")
+    quizz.classList.toggle("escondido")
+}
+
+function receberQuizzUnico () {
+    const quizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1")
+
+    quizz.then(perguntasRespostas)
+    quizz.catch(() => alert("ERRO2!"))
+}
+
+function perguntasRespostas (info) {
+    const quizz = info.data;
+    console.log(quizz.title)
+    const perguntas = document.querySelector(".quizzIndividual")
+
+    for (let i = 0; i < quizz.length; i++) {
+        perguntas.innerHTML = 
+        `
+        <div class="topoQuizz">
+            <img src="${quizz.image}"/>                   
+            <span class="tituloQuizzIndividual">
+                ${quizz.title}
+            </span>
+        </div>
+        <div class="pergunta">
+            <div class="tituloPergunta">
+            </div>
+            <div class="respostas">
+                <img src="">
+                <span class="tituloResposta"></span>
+            </div>
+        </div>
+        `
+    }
+}
+
+receberQuizzUnico();
